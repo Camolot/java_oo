94d8eb1aea4edfa5719a665d0f82c6b600f9ed51
Camolot <camolotthecreator@gmail.com>
2015-09-22 15:39:04 -0700

    add lost functionality, untested

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-var pizza = function(numeric) {
+var pizza = function(topping, size, quantity) {
 
     // var topping = 1.5;
     // var size = 3.5;
     // var quantity = parseInt(document.getElementById('getQuantity'),value, 10);
     // var quantity = 3;
 
-    $(document).ready(function() {
-      $("form#pizzaForm").submit(function(event) {
-        var topping = parseInt($("input#topping").val());
-        var size = parseInt($("input#size").val());
-        var quantity = parseInt($("input#quantity").val());
-        var total = (topping + size) * quantity;
-        var result = pizzaTotal(total);
-
-        $(".numeric").text(numeric);
-        if (!result) {
-          $(".not").text("not");
-        }
-
-        $("#result"),show();
-        event.preventDefault();
-      })
-    })
-
-    // var total = (topping + size) * quantity
+    var total = (topping + size) * quantity;
     return total;
 }
+
+$(document).ready(function() {
+  $("form#pizzaForm").submit(function(event) {
+    var t = parseInt($("input#t").val());
+    var s = parseInt($("input#s").val());
+    var q = parseInt($("input#q").val());
+    var total = (t + s) * q;
+
+    $(".numeric").text(numeric);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result"),show();
+    event.preventDefault();
+  });
+  return total;
+});
